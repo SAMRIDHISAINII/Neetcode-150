20d59461debe52ee93ab8620a606a0b1bbd9b0b8
Samridhi <83508209+SAMRIDHISAINII@users.noreply.github.com>
2022-12-26 20:17:19 +0530

    Update twosum.cpp

--- array/twosum.cpp
+++ array/twosum.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-//         so basucally we are given with an array of intefer nums
-//         basically if nums= 234 target 
-        vector<int> v;
-//         we nned to use inorfer tog
-     std::unordered_map<int, int> record;
-        for (int i = 0; i != nums.size(); ++i) 
+        // for solving yeh wali problem 
+        // iterate karo elemnts ko nums array fir each element x fir uske baad check target array mein
+
+        // uske baad agar toh target found hai toh return kardo indic of x saath meiontarget
+
+        // create karenge map for storing the indices of the element inarr
+        unordered_map<int,int> indices;
+
+        // iterate through the elements in the array 
+        for(int i=0; i<nums.size(); i++)
         {
-            auto found = record.find(nums[i]);
-            if (found != record.end())
-                
-                return {found->second, i};
-            record.emplace(target - nums[i], i);
+            int x = nums[i];
+             // Check if target - x is in the array
+        if (indices.count(target - x)) {
+            // If it is, return the indices of x and target - x
+            return { indices[target - x], i };
         }
-        return {-1, -1};
+
+        // Otherwise, add the index of x to the map
+        indices[x] = i;
+        }
+        // If we have not found a solution, return an empty vector
+    return {};
     }
 };
+
+// COMPLEXITY HOGI O(n)
