6ad1ffeb0f409bce8be4646921d5141b94394631
Samridhi <83508209+SAMRIDHISAINII@users.noreply.github.com>
2022-12-28 21:34:40 +0530

    Create Last Stone Weight.cpp

---
+++ heap/Last Stone Weight.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+           multiset<int> s(begin(stones), end(stones));
+        while (s.size() > 1) 
+        {
+            auto w1 = *prev(s.end());
+            s.erase(prev(s.end()));
+            auto w2 = *prev(s.end());
+            s.erase(prev(s.end()));
+            if (abs(w1 - w2) > 0) s.insert(abs(w1 - w2));
+        }
+        return s.empty() ? 0 : *s.begin();
+    }
+};
